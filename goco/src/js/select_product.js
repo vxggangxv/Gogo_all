83cc7cf3c40b56708cf98928af166e7204352bcf
Hye jun <vxggangxv@naver.com>
2017-02-28 12:01:41 +0900

    추가수정

--- goco/src/js/select_product.js
+++ goco/src/js/select_product.js
@@ -7,8 +7,39 @@ $(function() {
 		$(this).closest("#cal_wrap2").removeClass("on");
 		return false;
 	});
-	$("#fare_table .f_td").on("click", function() {
+	/*$("#fare_table .f_td, #day_table .f_td").on("click", function() {
 		$(this).toggleClass("on");
+	});*/
+	/*$("#day_table .f_td .c2 .p2 .in1").prev().on("click", function() {
+		var thVal = $(this).closest("div").find("input").val();
+		if( thVal > 0){
+			thVal --;
+			$(this).next().val(thVal);
+		}
+		var firstVal = $("#day_table .f_td .c2 div:eq(0) .p2 .in1").val();
+		var secondVal = $("#day_table .f_td .c2 div:eq(1) .p2 .in1").val();
+		console.log(firstVal);
+		console.log(secondVal);
+		if ( firstVal == 0 && secondVal == 0){
+			$(this).closest(".f_td").removeClass("on");
+		}
+		return false;
 	});
+	$("#day_table .f_td .c2 .p2 .in1").next().on("click", function() {
+		var thVal = $(this).closest("div").find("input").val();
+		if( thVal < 0){
+			thVal ++;
+			$(this).next().val(thVal);
+		}
+		$(this).closest(".f_td").addClass("on");
+		var firstVal = $("#day_table .f_td .c2 div:eq(0) .p2 .in1").val();
+		var secondVal = $("#day_table .f_td .c2 div:eq(1) .p2 .in1").val();
+		console.log(firstVal);
+		console.log(secondVal);
+		if ( firstVal == 0 && secondVal == 0){
+			$(this).closest(".f_td").removeClass("on");
+		}
+		return false;
+	});*/
 
 });
