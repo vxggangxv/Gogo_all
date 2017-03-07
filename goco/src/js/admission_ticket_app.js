1a8e83a14f0bac743d81a4fcedfcfc5503ed06ef
Hye jun <vxggangxv@naver.com>
2017-03-07 20:27:56 +0900

    코코앱UI수정

--- goco/src/js/admission_ticket_app.js
+++ goco/src/js/admission_ticket_app.js
@@ -7,4 +7,16 @@ $(function() {
 		$(this).find("input").prop("checked", isChk);
 		//console.log($(this).find("input").prop("checked"));
 	});
+	/* 드롭다운 */
+  $(".dropdown .btn").on("click", function() {
+    $(this).next().slideToggle("fast");
+  });
+  $(".dropdown-menu > li > a").on("click", function() {
+  	$(this).closest("ul").slideToggle("fast");
+    var thTxt = $(this).text();
+    //console.log(thTxt);
+    var thHtml = thTxt + '<i class="fa fa-caret-down fa-lg" aria-hidden="true"></i>' ;
+    $(this).closest("div").find("button").html(thHtml);
+
+  });
 });
