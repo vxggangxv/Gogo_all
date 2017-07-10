46420e43ac356406c19ad889b035e8d1f7a624fc
Hye jun <vxggangxv@naver.com>
2017-07-10 16:10:26 +0900

    모바일추천인적립금페이지완료

--- goco/src/js/m/proceeds/proceeds.js
+++ goco/src/js/m/proceeds/proceeds.js
@@ -1,8 +1,18 @@
-$(function() {
-  //랭킹 텝 버튼
-  $("#fr-rankbox li a").on("click", function() {
-    var idx = $(this).parent().index();
-    $(this).closest("ul").find("li").eq(idx).addClass("on").siblings().removeClass("on");
-  	return false;
-  });
+$(function () {
+	//랭킹 텝 버튼
+	$("#fr-rankbox li a").on("click", function () {
+		var idx = $(this).parent().index();
+		$(this).closest("ul").find("li").eq(idx).addClass("on").siblings().removeClass("on");
+		return false;
+	});
+	//수익금 택 버튼
+	$("#tab li").on("click", function() {
+		var idx = $(this).index();
+		$(this).addClass("on").siblings().removeClass("on");
+		$("#tab-con").find("li").eq(idx).addClass("on").siblings().removeClass("on");
+	});
+	//공유버튼 클릭
+	$("#promotion-sec .i-share").on("click", function() {
+		$("#bg-drop, #q-modal").show();
+	});
 });
