aa4dd77245e70f4e30232fca1e273c19555307e9
강혜준 <vxggangxv@naver.com>
2018-04-26 14:10:46 +0900

    1대1문의 수정중

--- goco_brand/hotelgangneung.kr/src/js/init.js
+++ goco_brand/hotelgangneung.kr/src/js/init.js
@@ -1,25 +1,6 @@
 $(function() {
 	//FAQ 노출, 질문 펼치기 및 닫기
-	$("#faq-a").on('click', function() {
-		$('body').css({
-			"position": "fixed",
-			"width": "100%"
-		});
-		$("#faqWrap-back").fadeIn();
-		$(this).addClass('on');
-	});
-	$("#faqWrap .i-x").on('click', function() {
-		$('body').css({
-			"position": "relative",
-			"width": "100%"
-		});
-		$("#faqWrap-back").hide();
-		$("#faq-a").removeClass('on');
-	});
-	$("#faqWrap .d1").click(function() {
-		$(this).next().slideToggle('fast');
-		 $(this).toggleClass('on');
-	});
+	faq();
 	// 모바일
 	/*$("#faq .f_q").on("click", function () {
 		$(this).next().slideToggle("fast");
@@ -28,3 +9,35 @@ $(function() {
 		$('#faq li').show();
 	});*/
 });
+
+function faq() {
+	var wrap = $("#faqWrap"),
+		wrapBack = $("#faqWrap-back"),
+		faqBtn = $("#faq-a");
+	faqBtn.on('click', function() {
+		$('body').css({
+			"position": "fixed",
+			"width": "100%"
+		});
+		wrapBack.fadeIn();
+		$(this).addClass('on');
+	});
+	wrap.find(".i-x").on('click', function() {
+		$('body').css({
+			"position": "relative",
+			"width": "100%"
+		});
+		wrapBack.hide();
+		faqBtn.removeClass('on');
+	});
+	wrap.find(".d1").click(function() {
+		$(this).next().slideToggle('fast');
+		 $(this).toggleClass('on');
+	});
+	wrap.find(".faq-tab li").click(function() {
+		var idx = $(this).index();
+		$(this).addClass("on").siblings().removeClass("on");
+		wrap.find(".contents-tab > .faq-main").eq(idx).show().siblings().hide();
+		console.log(idx);
+	});
+}
